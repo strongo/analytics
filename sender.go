2ab81e3b898d5ec6e45e86c985d6223077b4a596
Alexander Trakhimenok <alex@trakhimenok.com>
2025-07-16 12:29:28 +0100

    fix: improved QueueMessage() - check for nil + better error logging

--- sender.go
+++ sender.go
@@ -12,8 +12,14 @@ type Sender interface {
 var senders []Sender
 
 func QueueMessage(ctx context.Context, msg Message) {
+	if ctx == nil {
+		panic("analytics.QueueMessage(ctx=nil)")
+	}
+	if msg == nil {
+		panic("analytics.QueueMessage(msg=nil)")
+	}
 	if err := msg.Validate(); err != nil {
-		logus.Errorf(ctx, "queued invalid message to analytics: %s", err)
+		logus.Errorf(ctx, "analytics.QueueMessage() receieved an invalid message, validation error %T: %s", err, err)
 		return
 	}
 	for _, sender := range senders {
